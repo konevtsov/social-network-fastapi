bf00e9b2251492114a19fbc3a4580f455bd371ad
Egor Konevtsov <konevtsov.code@gmail.com>
2024-07-14 20:50:49 +0700

    Fix swagger download problem

--- app/main.py
+++ app/main.py
@@ -1,36 +1,17 @@
-from contextlib import asynccontextmanager
-
 import uvicorn
-from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
-from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
-from core.models import db_helper
 
+from api import router as api_router
+from create_fastapi_app import create_app
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # startup
-    yield
-    # shutdown
-    await db_helper.dispose()
-
-
-main_app = FastAPI(
-    # title=settings.run.title,
-    default_response_class=ORJSONResponse,
-    lifespan=lifespan,
+main_app = create_app(
+    create_custom_static_urls=True,
 )
 
-# main_app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=list(settings.run.BACKEND_CORS_ORIGINS),
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
+main_app.include_router(
+    api_router,
+)
 
 if __name__ == "__main__":
     uvicorn.run(
