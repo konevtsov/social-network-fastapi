f0e2dbebc3ceb46ce507b9d0573feb41eee91733
Egor Konevtsov <konevtsov.code@gmail.com>
2024-07-14 11:38:34 +0700

    Create profiles table

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,7 +1,12 @@
 __all__ = (
     "db_helper",
     "Base",
+    "User",
+    "Post",
 )
 
 from .db_helper import db_helper
 from .base import Base
+from .user import User
+from .post import Post
+from .profile import Profile
